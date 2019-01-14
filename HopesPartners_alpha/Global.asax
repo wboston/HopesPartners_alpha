417fe7d1e88c84458358316b7bd7fe10fe9dde49
William <williamboston123@hotmail.com>
2019-01-13 18:40:31 -0800

    Add project files.

---
+++ HopesPartners_alpha/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HopesPartners_alpha.MvcApplication" Language="C#" %>
---
+++ HopesPartners_alpha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HopesPartners_alpha.Startup))]
+namespace HopesPartners_alpha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
