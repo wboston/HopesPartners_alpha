05801ba52c25c1f34a52508cc999df79e75755e8
William <williamboston123@hotmail.com>
2019-01-17 20:36:21 -0800

    Add base Properties To UserModels cls
    UserModels extends ApplicationUser.Email and adds DateTime and a Test Role Attributes

--- HopesPartners_alpha/Models/UserModels.cs
+++ HopesPartners_alpha/Models/UserModels.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HopesPartners_alpha.Models
 {
-    public class UserModels
+    public class UserModels : ApplicationUser
     {
+        [Required]
+        [EmailAddress]
+        public override string Email { get; set; }
+
+        [Display(Name = "Partner Since")]
+        public string DateTime { get; set; }
+
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+
+        private List<string> _roles = new List<string>() { "Partner", "Partner" };
+        public IEnumerable<SelectListItem> RoleTypes
+        {
+            get { return new SelectList(_roles); }
+        }
     }
 }
