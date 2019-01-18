ea58643d57c89df0a816c08c8292d4a6333004f3
William <williamboston123@hotmail.com>
2019-01-17 19:53:58 -0800

    Add Basic Properties to the Content Class

--- HopesPartners_alpha/Models/ContentModels.cs
+++ HopesPartners_alpha/Models/ContentModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,14 @@ namespace HopesPartners_alpha.Models
 {
     public class ContentModels
     {
+        [Display(Name ="Title of Document")]
+        public String Title { get; set; }
+
+        [Display(Name ="Link")]
+        public String WebAddress { get; set; }
+
+        [Display(Name ="Date Posted")]
+        public DateTime DateCreated { get; set; }
+
     }
 }
