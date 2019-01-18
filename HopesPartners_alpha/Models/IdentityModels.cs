d4f3d53f56baaac5e0551584efbee5ac7a32926a
William <williamboston123@hotmail.com>
2019-01-18 00:41:59 -0800

    Add
    other Filler information into default Application User

--- HopesPartners_alpha/Models/IdentityModels.cs
+++ HopesPartners_alpha/Models/IdentityModels.cs
@@ -9,6 +9,12 @@ namespace HopesPartners_alpha.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FamilyName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -29,5 +35,8 @@ namespace HopesPartners_alpha.Models
         {
             return new ApplicationDbContext();
         }
+        
+        public System.Data.Entity.DbSet<HopesPartners_alpha.Models.UserModels> ApplicationUsers { get; set; }
+
     }
 }
